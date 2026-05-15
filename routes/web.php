25fafdc1abb921ba6c01750f57cb8fc15ecf8330
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-05-15 18:01:11 +0100

    feat(routes): protect all app routes with auth middleware, keep only login/logout

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('products', ProductController::class);
+    Route::get('invoices/{invoice}/download', [App\Http\Controllers\InvoiceController::class, 'download'])->name('invoices.download');
+    Route::resource('invoices', App\Http\Controllers\InvoiceController::class)->except(['edit', 'update']);
+});
 
-Route::resource('products', ProductController::class);
-Route::get('invoices/{invoice}/download', [App\Http\Controllers\InvoiceController::class, 'download'])->name('invoices.download');
-Route::resource('invoices', App\Http\Controllers\InvoiceController::class)->except(['edit', 'update']);
+require __DIR__.'/auth.php';
