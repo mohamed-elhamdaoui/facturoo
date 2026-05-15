ae09c90ce1e1dadc25ef016f3e8b61e067effa3c
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-05-15 18:23:16 +0100

    fix(auth): recreate GuestLayout component class deleted during cleanup

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" value="Adresse Email" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" value="Mot de passe" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-6">
+            <x-primary-button class="ms-3 w-full justify-center">
+                Se connecter
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
