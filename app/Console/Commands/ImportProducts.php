538bb0f9f759e07866960217785befffc659c85d
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-06-25 05:49:38 +0100

    Add ImportProducts command

---
+++ app/Console/Commands/ImportProducts.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ImportProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import products from backup_clean.sql';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $file = base_path('backup_clean.sql');
+        
+        if (!file_exists($file)) {
+            $this->error("backup_clean.sql not found!");
+            return;
+        }
+
+        $sql = file_get_contents($file);
+
+        // Find the INSERT INTO `products` query
+        if (preg_match('/INSERT INTO `products` VALUES .*?;/s', $sql, $matches)) {
+            $query = $matches[0];
+            
+            // Clean up DB before inserting to avoid duplicates if it was already imported
+            DB::table('products')->truncate();
+            
+            DB::unprepared($query);
+            $this->info("Products imported successfully from backup!");
+        } else {
+            $this->error("Could not find the products INSERT statement in the SQL file.");
+        }
+    }
+}
